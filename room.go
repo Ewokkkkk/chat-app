da50980cf57d1834c37bccdcfe8d1776b8b4d5a7
Ewokkkkk <ewok.mfk@gmail.com>
2020-08-23 17:19:08 +0900

    34

--- room.go
+++ room.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"trace"
 
 	"github.com/gorilla/websocket"
 )
@@ -16,6 +17,8 @@ type room struct {
 	leave chan *client
 	//	在室している全てのクライアントが保持される
 	clients map[*client]bool
+	// チャットルーム場で行われた操作のログを受け取る
+	tracer trace.Tracer
 }
 
 // チャットルームを生成して返す
@@ -25,6 +28,7 @@ func newRoom() *room {
 		join:    make(chan *client),
 		leave:   make(chan *client),
 		clients: make(map[*client]bool),
+		tracer:  trace.Off(),
 	}
 }
 
@@ -36,20 +40,25 @@ func (r *room) run() {
 		case client := <-r.join:
 			//	参加
 			r.clients[client] = true
+			r.tracer.Trace("新しいクライアントが参加しました")
 		case client := <-r.leave:
 			//	退室
 			delete(r.clients, client)
 			close(client.send)
+			r.tracer.Trace("クライアントが退室しました")
 		case msg := <-r.forward:
+			r.tracer.Trace("メッセージを受信しました：", string(msg))
 			//	全てのクライアントにメッセージを転送
 			for client := range r.clients {
 				select {
 				case client.send <- msg:
 					//	メッセージ送信
+					r.tracer.Trace(" -- クライアントに送信しました")
 				default:
 					//	送信失敗
 					delete(r.clients, client)
 					close(client.send)
+					r.tracer.Trace(" -- 送信に失敗しました。クライアントをクリーンアップします")
 				}
 			}
 		}
