44bb9541f5b1e865e2da6056a2078ca5939b36a4
Ewokkkkk <ewok.mfk@gmail.com>
2020-08-24 23:00:24 +0900

    loginHandlerの実装

--- auth.go
+++ auth.go
@@ -1,9 +1,45 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"strings"
 )
+
 type authHandler struct {
 	next http.Handler
 }
-func (h *authHandler) ServeHTTP
+
+func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if _, err := r.Cookie("auth"); err == http.ErrNoCookie {
+		//	未認証
+		w.Header().Set("Location", "/login")
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	} else if err != nil {
+		//	何らかのエラーが発生
+		panic(err.Error())
+	} else {
+		// 成功。ラップされたハンドラを呼び出す
+		h.next.ServeHTTP(w, r)
+	}
+}
+func MustAuth(handler http.Handler) http.Handler {
+	return &authHandler{next: handler}
+}
+
+// loginHandlerはサードパーティーへのログインの処理を受け持つ
+// パスの形式: localhost/auth/{action}/{provider}
+//			  segs[0]/segs[1]/segs[2]/segs[3]
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	segs := strings.Split(r.URL.Path, "/")
+	action := segs[2]
+	provider := segs[3]
+	switch action {
+	case "login":
+		log.Println("TODO: ログイン処理", provider)
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "アクション%sには非対応です", action)
+	}
+}
--- main.go
+++ main.go
@@ -29,7 +29,9 @@ func main() {
 	flag.Parse() // フラグを解釈する
 	r := newRoom()
 	// r.tracer = trace.New(os.Stdout)
-	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
+	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.HandleFunc("/auth/", loginHandler)
 	http.Handle("/room", r)
 
 	// チャットルームを開始(goroutineとして実行)
