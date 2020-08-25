63253eef752991f8d671e8729ea13dcd88f2a304
Ewokkkkk <ewok.mfk@gmail.com>
2020-08-26 04:30:07 +0900

    Add : OAuth2認証の骨組み（googleのみ）
    google OAuth2のコールバックを返す部分のみ

--- main.go
+++ main.go
@@ -7,6 +7,9 @@ import (
 	"path/filepath"
 	"sync"
 	"text/template"
+
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/google"
 )
 
 type templateHandler struct {
@@ -27,6 +30,10 @@ func main() {
 	// -addr=":8080" が初期値
 	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
 	flag.Parse() // フラグを解釈する
+	gomniauth.SetSecurityKey("key")
+	gomniauth.WithProviders(
+		google.New("996339149681-vgorqnrenqeoukg6ecdbf4odlig0a72e.apps.googleusercontent.com", "EVLast0qPHtyJq5nPXG54B0O", "http://localhost:8080/auth/callback/google"),
+	)
 	r := newRoom()
 	// r.tracer = trace.New(os.Stdout)
 	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
