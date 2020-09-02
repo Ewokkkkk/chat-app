dea5b47498b8648cfb4ccb86d7c3dd42e0036bd2
Ewokkkkk <ewok.mfk@gmail.com>
2020-09-02 14:40:34 +0900

    add: Gravatarアイコン取得の実装
    Gomniauthからログインしたアカウントのメールアドレスを取得し、そのアドレスのハッシュ値を元にGravatarのアバター画像URLを取得し、セットする

--- avatar.go
+++ avatar.go
@@ -1,6 +1,12 @@
 package main
 
-import "errors"
+import (
+	"crypto/md5"
+	"errors"
+	"fmt"
+	"io"
+	"strings"
+)
 
 //  ErrNoAvatarはAvatarインスタンスがアバターのURLを返すことが出来ない場合に発生するエラーです
 var ErrNoAvatarURL = errors.New("chat: アバターのURLを取得できません。")
@@ -21,6 +27,21 @@ func (_ AuthAvatar) GetAvatarURL(c *client) (string, error) { // レシーバー
 	if url, ok := c.userData["avatar_url"]; ok {
 		if urlStr, ok := url.(string); ok {
 			return urlStr, nil
+		}
+	}
+	return "", ErrNoAvatarURL
+}
+
+type GravatarAvatar struct{}
+
+var UseGravatar GravatarAvatar
+
+func (_ GravatarAvatar) GetAvatarURL(c *client) (string, error) {
+	if email, ok := c.userData["email"]; ok {
+		if emailStr, ok := email.(string); ok {
+			m := md5.New()
+			io.WriteString(m, strings.ToLower(emailStr))
+			return fmt.Sprintf("//www.gravatar.com/avatar/%x", m.Sum(nil)), nil
 		}
 	}
 	return "", ErrNoAvatarURL
