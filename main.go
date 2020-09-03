d73fd39f4040428c95ffe66ded146fcd26483c43
Ewokkkkk <ewok.mfk@gmail.com>
2020-09-04 08:26:33 +0900

    add: アップロードした画像をアバター画像として使用する
    ファイルサーバーで/avatarsディレクトリを公開し、画像のurlをアバターurlとして利用できるようにした

--- main.go
+++ main.go
@@ -42,7 +42,7 @@ func main() {
 	gomniauth.WithProviders(
 		google.New(clientIDGoogle, clientSecretGoogle, "http://localhost:8080/auth/callback/google"),
 	)
-	r := newRoom(UseGravatar)
+	r := newRoom(UseFileSystemAvatar)
 	// r.tracer = trace.New(os.Stdout)
 	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
 	http.Handle("/login", &templateHandler{filename: "login.html"})
@@ -60,6 +60,9 @@ func main() {
 	})
 	http.Handle("/upload", &templateHandler{filename: "upload.html"})
 	http.HandleFunc("/uploader", uploaderHandler)
+	http.Handle("/avatars/",
+		http.StripPrefix("/avatars",
+			http.FileServer(http.Dir("./avatars"))))
 
 	// チャットルームを開始(goroutineとして実行)
 	go r.run()
